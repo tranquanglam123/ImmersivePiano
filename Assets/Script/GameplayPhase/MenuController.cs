f0b193b21c34e39cac5daef8ca59c1e2b3a184e8
QuangLam <kwanglum815@gmail.com>
2023-12-11 22:52:49 +0700

    Done Config spawning / notes highlighting

---
+++ Assets/Script/GameplayPhase/MenuController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Menu Objects")]
+    [SerializeField] GameObject intro;
+    [SerializeField] GameObject edgeConfirm;
+    [SerializeField] GameObject resetEdge;
+    [SerializeField] GameObject songsSelection;
+    [SerializeField] GameObject songSheets;
+}
